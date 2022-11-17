5b2ac1ec210a94a54e24c72847591d20ad0af015
chayan <ya.giggs2013@yandex.ru>
2022-11-18 02:16:12 +0300

    fixed some repeated code

--- main_menu/urls.py
+++ main_menu/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', Menu.as_view()),
-    path('contacts/', contacts),
-    path('sale/', Sale.as_view()),
-    path('rent/', Rent.as_view()),
-    path('about_us/', about_us),
-    path('medical_insurance/', medical_insurance),
-    path('resident_card/', resident_card),
+    path('', Menu.as_view(), name='all_objects'),
+    path('contacts/', contacts, name='contacts'),
+    path('sale/', Sale.as_view(), name='sale_list'),
+    path('rent/', Rent.as_view(), name='rent_list'),
+    path('about_us/', about_us, name='about_us'),
+    path('medical_insurance/', medical_insurance, name='medical_insurance'),
+    path('resident_card/', resident_card, name='resident_card'),
 ]
