13b26ca0672883e2e3641a8e8577c1b5906a43aa
chayan <ya.giggs2013@yandex.ru>
2022-12-01 21:25:27 +0300

    added new class in model py, old one was deleted

--- main_menu/admin.py
+++ main_menu/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
-from .models import RentSale
+
+from .models import *
+
+
+class PictureInline(admin.StackedInline):
+    model = Picture
 
 
 @admin.register(RentSale)
 class SiteAdmin(admin.ModelAdmin):
-    list_display = ['city', 'type', 'district', 'address', 'owner', 'phone_number', 'price_in_lira', 'image']
-    ordering = ['district']
-    list_per_page = 10
+    #     # list_display = ['city', 'type', 'district', 'address', 'owner', 'phone_number', 'price_in_lira']
+    #     # ordering = ['district']
+    #     # list_per_page = 10
+    inlines = [PictureInline]
+    pass
+
+
+@admin.register(Picture)
+class SiteAdmin(admin.ModelAdmin):
+    #     # list_display = ['image']
+    #     list_per_page = 100
+    pass
